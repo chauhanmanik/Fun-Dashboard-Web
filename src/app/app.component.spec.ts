03d9382524b9cbbc85ff7a6ee907f1d05d939f15
chauhanmanik <chauhanmanik@hotmail.com>
2024-05-04 02:44:10 +0100

    Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'Fun-Dashboard-Web' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Fun-Dashboard-Web');
+    expect(app.title).toEqual('Fun-Dashboard-Web Deploy Hurrah');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Fun-Dashboard-Web');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, Fun-Dashboard-Web Deploy Hurrah'
+    );
   });
 });
