5425846936e9b09a404fd9990be1dcd5e922fd31
chauhanmanik <chauhanmanik@hotmail.com>
2024-05-11 22:01:55 +0100

    - Test ActivatedRoute error fixed

--- src/app/content/content.component.spec.ts
+++ src/app/content/content.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContentComponent } from './content.component';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 describe('ContentComponent', () => {
   let component: ContentComponent;
@@ -8,10 +10,15 @@ describe('ContentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContentComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        ContentComponent,
+        RouterModule.forRoot([
+          { path: '', component: DashboardComponent },
+          { path: 'content', component: ContentComponent },
+        ]),
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ContentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
-// import { ContentModule } from './content.module';
 
 @Component({
   selector: 'fun-content',
